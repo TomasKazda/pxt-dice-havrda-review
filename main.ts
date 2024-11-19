81e777e358d9440c20d77c7088b314df86b4604d
Tomáš Kazda <36702826+TomasKazda@users.noreply.github.com>
2024-11-19 21:38:42 +0100

    corrections

--- main.ts
+++ main.ts
@@ -4,33 +4,36 @@ const dicemin: number = 2;
 let decision: boolean = true;
 
 while (true) {
-    while (decision === true) {
+    if (decision) {
         if (input.buttonIsPressed(Button.A)) {
             if (dicesides > dicemin) {
-                dicesides = dicesides - 1;
-                basic.showNumber(dicesides);
-                music.play(music.tonePlayable(Note.D, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+                dicesides -= 1;
+                whaleysans.showNumber(dicesides);
+                music.play(music.tonePlayable(Note.D, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
             }
+
         }
         if (input.buttonIsPressed(Button.B)) {
             if (dicesides < dicemax) {
-                dicesides = dicesides + 1;
-                basic.showNumber(dicesides);
-                music.play(music.tonePlayable(Note.C3, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+                dicesides += 1;
+                whaleysans.showNumber(dicesides);
+                music.play(music.tonePlayable(Note.C3, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
             }
         }
         if (input.isGesture(Gesture.Shake)) {
-            let dicerandom = randint(dicemin, dicesides);
+            let dicerandom = randint(1, dicesides);
             basic.showNumber(dicerandom);
-            music.play(music.tonePlayable(Note.C, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+            music.play(music.tonePlayable(Note.C, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
             decision = false
         }
     }
 
-    while (decision === false) {
+    if (!decision) {
         if (input.logoIsPressed()) {
             decision = true
-            music.play(music.tonePlayable(Note.C3, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+            music.play(music.tonePlayable(Note.C3, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
         }
     }
+
+    basic.pause(50)
 }
